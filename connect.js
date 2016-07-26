3c64cc33ad7db6b9918a17ffc63f8fa31acb3ef1
Michael O'Hare <mtohare16@gmail.com>
2016-07-26 17:59:20 -0400

    First upload

---
+++ connect.js
@@ -0,0 +1,25 @@
+/*var x = document.getElementsByClassName('headline');
+
+//x=3;
+
+//var x = 3
+console.log(x)
+//debugger;
+var n = 2;
+if (n % 2 === 0 ) {
+  console.log("n = " + n);
+  console.log("even")
+};
+*/
+
+var gameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 500;
+    this.canvas.height = 270;
+
+  }
+}
+
+
+
