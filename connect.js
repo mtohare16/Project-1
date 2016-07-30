40cd21f9de989cc37508cf05ecde73538130eae8
Michael O'Hare <mtohare16@gmail.com>
2016-07-30 13:17:01 -0400

    Added an event tracker using clicks

--- connect.js
+++ connect.js
@@ -1,25 +1,31 @@
-/*var x = document.getElementsByClassName('headline');
 
-//x=3;
-
-//var x = 3
-console.log(x)
-//debugger;
-var n = 2;
-if (n % 2 === 0 ) {
-  console.log("n = " + n);
-  console.log("even")
-};
-*/
-
-var gameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = 500;
-    this.canvas.height = 270;
-
-  }
-}
+document.addEventListener("click", function(evnt) {
+  console.log(evnt.target);
+  //reset?
 
 
+
+  //get a column
+  var eventTarget = evnt.target;
+  var classes = eventTarget.classList; //['c7']
+  var col = classes[0]
+
+  console.log(col);
+  var piece = col[0]
+
+  //place a piece in the column
+  //placePiece(col);
+  //
+});
+
+//if cd[x][i]
+  //ig i |= 0
+//function placePiece()
+//check for nearest spot to the bottom
+// col = 1
+//loop for each column to figure out where the piece would go
+///inside the loop check if space is empty
+
+
+//var board [?][1] //1 is the column ? is the row number
 
