ac677d77f7e0d0598b21a196504eb389a7c0b688
Michael O'Hare <mtohare16@gmail.com>
2016-08-01 16:53:06 -0400

    Got pieces to move to bottom row

--- connect.js
+++ connect.js
@@ -1,22 +1,36 @@
 
 document.addEventListener("click", function(evnt) {
-  console.log(evnt.target.className);
-  //reset?
+  //console.log(evnt.target.classList);
 
-var target = evnt.target.className;
-document.getElementsByClassName(evnt.target.className).style.backgroundColor;
+  var columnName = evnt.target.classList[0];
+  var rowName = 'r6';
+  var piecePlace = columnName + " " + rowName;
+  var temp = document.getElementsByClassName(piecePlace)[0];
 
-  //var black = document.getElementById(evnt.target.id).style.backgroundColor = 'black';
-var eventTarget = evnt.className;
+  console.log(temp);
+  var red = temp.style.backgroundColor = 'red';
+  //create a function for above
+  //create data structure
+/*function(checkForPlacement) {
+  var check = checkForPlacement.target.classList[0];
+  console.log(check);
+}*/
+
+  /*checkforplacement (column number)
+//
+  var selectedElem
+  var col = selectedElem[0]
+  var row = selectedElem[0]
+  var filled = selectedElem[2]*/
+
+
+//var red = document.getElementsByClassName(evnt.target.className);
+//console.log(red);
+//var black = document.getElementById(evnt.target.id).style.backgroundColor = 'black';
+//var eventTarget = evnt.className;
 
 
 //var elements = document.getElementByTagName('*');
-
-for(var i=0 ; i<elements.length ; i++){
-  elements[i].addEventListener('click', red, false);
-}
-
-
 
   //this.//get the function to work with buttons
   //var piece = col[0]
@@ -25,10 +39,6 @@ for(var i=0 ; i<elements.length ; i++){
   //placePiece(col);
   //
 });
-
-
-
-
 
 /*create two players for the game,
 player 1 will be red circles, and player 2 will be black circles*/
